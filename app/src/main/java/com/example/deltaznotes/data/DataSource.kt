bcf61570e81956a87e737ca4bdefcb08aa048204
AS-Coope <deltazirconium@gmail.com>
2023-07-16 11:59:08 -0500

    Start Data Source creation to load notes properly

---
+++ app/src/main/java/com/example/deltaznotes/data/DataSource.kt
@@ -0,0 +1,14 @@
+package com.example.deltaznotes.data
+
+import com.example.deltaznotes.model.Note
+
+// take the various parts of the data (basically the Note is a data structure)
+// and format it so that it appears as a note should, on screen
+class DataSource {
+
+    fun loadNotes(): List<Note>{
+        return listOf(
+
+        )
+    }
+}
