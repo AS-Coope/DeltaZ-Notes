2594a660eb092a51613456c9d57aa2e465593c7d
AS-Coope <deltazirconium@gmail.com>
2023-07-16 12:13:31 -0500

    Update string.xml with note headings and contents and added Notes to Data Source class

--- app/src/main/java/com/example/deltaznotes/data/DataSource.kt
+++ app/src/main/java/com/example/deltaznotes/data/DataSource.kt
@@ -1,5 +1,6 @@
 package com.example.deltaznotes.data
 
+import com.example.deltaznotes.R
 import com.example.deltaznotes.model.Note
 
 // take the various parts of the data (basically the Note is a data structure)
@@ -8,7 +9,13 @@ class DataSource {
 
     fun loadNotes(): List<Note>{
         return listOf(
-
+            Note(R.string.note1_heading, R.string.note1_contents, false),
+            Note(R.string.note2_heading, R.string.note2_contents, false),
+            Note(R.string.note3_heading, R.string.note3_contents, false),
+            Note(R.string.note4_heading, R.string.note4_contents, false),
+            Note(R.string.note5_heading, R.string.note5_contents, false),
+            Note(R.string.note6_heading, R.string.note6_contents, false),
+            Note(R.string.note7_heading, R.string.note7_contents, false)
         )
     }
 }
