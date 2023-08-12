60a1f560fc1b39ae5af30a31585d18b77773401a
AS-Coope <deltazirconium@gmail.com>
2023-08-12 12:05:33 -0500

    Set up view binding in Details Activity

--- app/src/main/java/com/example/deltaznotes/model/DetailsActivity.kt
+++ app/src/main/java/com/example/deltaznotes/model/DetailsActivity.kt
@@ -3,10 +3,14 @@ package com.example.deltaznotes.model
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.deltaznotes.R
+import com.example.deltaznotes.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
