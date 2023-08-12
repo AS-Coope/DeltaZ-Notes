f9832fb0e27462f524e36ae4140cb9cf717ff261
AS-Coope <deltazirconium@gmail.com>
2023-08-12 12:03:48 -0500

    Create DetailsActivity and details activity layout

---
+++ app/src/main/java/com/example/deltaznotes/model/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.deltaznotes.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deltaznotes.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
