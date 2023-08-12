2d63acaaa630609eb8608d39b33de163dde4a1f7
AS-Coope <deltazirconium@gmail.com>
2023-08-12 16:24:14 -0500

    Show note details for specific note clicked in the recycler view

--- app/src/main/java/com/example/deltaznotes/model/DetailsActivity.kt
+++ app/src/main/java/com/example/deltaznotes/model/DetailsActivity.kt
@@ -1,16 +1,29 @@
 package com.example.deltaznotes.model
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.deltaznotes.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.deltaznotes.data.DataSource
 import com.example.deltaznotes.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
+    private val loadedNotes = DataSource().loadNotes()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // acquiring the extra from the intent passed when the note in the recycler view was clicked
+        val noteDetails = intent.getIntExtra("NOTE", -1)
+
+        // searching for the specific note clicked in the recycler view
+        fetchNotes(noteDetails)
+    }
+
+    private fun fetchNotes(notePosition: Int){
+        // show information for the specific note clicked in the details activity
+        binding.tvNoteTitle.setText(loadedNotes[notePosition].headingResourceId)
+        binding.tvNoteDetails.setText(loadedNotes[notePosition].contentResourceId)
     }
 }
